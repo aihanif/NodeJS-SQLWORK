68d696bd5457e6cbe7c0da5b94985395dd3c3ee0
hanif <hanif@omesti.com>
2025-07-16 21:30:35 +0800

    Initial commit by hanif

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+
+
+///////////////////////////////////////////////////////////
+// Set view engine to EJS
+app.set('view engine', 'ejs');
+
+// Static files
+app.use(express.static('public'));
+
+// Routes
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Index' });
+});
+
+app.get('/sqlwork', (req, res) => {
+    res.render('sqlwork', { title: 'Work - SQL' });
+});
+
+// Start server
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`✅ Server running at http://localhost:${PORT}`);
+});
+///////////////////////////////////////////////////////////
+
+
+
